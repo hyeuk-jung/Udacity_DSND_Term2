b80b7c92116a8d262989a9ad0911cc27e4306c56
hyeuk-jung <hyeuk.jung.2019@anderson.ucla.edu>
2020-01-28 21:26:32 -0800

    First commit - ML pipeline and Web App

--- Part 9. Data Engineering/Project: Disaster Response Pipelines/models/train_classifier.py
+++ Part 9. Data Engineering/Project: Disaster Response Pipelines/models/train_classifier.py
@@ -1,24 +1,97 @@
 import sys
+import nltk
+import numpy as np
+import pickle
+import pandas as pd
+import re
 
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize, sent_tokenize
+
+from sklearn.base import BaseEstimator, TransformerMixin # For custom transformer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.metrics import confusion_matrix, classification_report, accuracy_score, precision_score, recall_score, f1_score
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.pipeline import Pipeline, FeatureUnion
+
+from sqlalchemy import create_engine
+
+nltk.download(['punkt', 'wordnet', 'stopwords'])
 
 def load_data(database_filepath):
-    pass
+    engine = create_engine('sqlite:///' + database_filepath)
+    df = pd.read_sql_table('msg_categories', con = engine)
+
+    X = df.message
+    Y = df.drop(['id', 'message', 'original', 'genre'], axis = 1)
+    category_names = Y.columns
+
+    return X, Y, category_names
 
 
 def tokenize(text):
-    pass
+    # normalize case and remove punctuation
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
+    
+    # tokenize text
+    tokens = word_tokenize(text)
+    
+    # lemmatize and remove stop words
+    stop_words = stopwords.words("english")
+    lemmatizer = WordNetLemmatizer()
+    tokens = [lemmatizer.lemmatize(word) for word in tokens if word not in stop_words]
+
+    return tokens
 
 
 def build_model():
-    pass
+    # build a ml pipeline
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer = tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier(), n_jobs = -1))
+    ])
+
+    # set a dictionary of parameters for hyperparameter tuning
+    parameters = {'clf__estimator__max_depth': [2, 3, 5], \
+                  'clf__estimator__min_samples_split': [2, 3, 5], \
+                  'clf__estimator__n_estimators': [50, 100]}
+
+    # create a model
+    model = GridSearchCV(pipeline, param_grid = parameters, cv = 3, \
+                         refit = True, verbose = 5) # , scoring = 'f1'
+    
+    return model
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    # predict the result
+    Y_pred = model.predict(X_test)
+    Y_pred = pd.DataFrame(Y_pred, columns = category_names)
+
+    # result = []
+    # for column in category_names:
+    #     accuracy = accuracy_score(Y_test[column], Y_pred[column])
+    #     precision = precision_score(Y_test[column], Y_pred[column], average = 'micro') 
+    #     recall = recall_score(Y_test[column], Y_pred[column], average = 'micro')
+    #     f1 = f1_score(Y_test[column], Y_pred[column], average = 'micro')
+        
+    #     result.append([accuracy, precision, recall, f1])
+        
+    # result = pd.DataFrame(result, index = category_names, \
+    #                       columns = ['Accuracy', 'Precision', 'Recall', 'F1'])
+    # print(result)          
+            
+    print(classification_report(Y_test.values, Y_pred.values, target_names = category_names), \
+          '\nAccuracy: {}'.format(accuracy_score(Y_test.values, Y_pred.values)) )
 
 
 def save_model(model, model_filepath):
-    pass
+    # filename = 'ML_pipeline_tunedmodel.pkl'
+    pickle.dump(model, open(model_filepath, 'wb')) 
 
 
 def main():
